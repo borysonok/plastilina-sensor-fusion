2a642bf6d36ff643a75d05fb868217586d4e6f98
Alex Borysonok <alex.borysonok@gmail.com>
2026-04-08 15:54:17 -0400

    [3] (~) Added proper formatting and other code tweaks

--- Plastilina/csv_writer.h
+++ Plastilina/csv_writer.h
@@ -2,7 +2,7 @@
 
 #include "true_path_sample.h"
 
-#include <vector>
 #include <string>
+#include <vector>
 
 void saveCSV(const std::vector<TruePathSample>& i_data, const std::string& i_filename);
--- Plastilina/main.cpp
+++ Plastilina/main.cpp
@@ -1,13 +1,22 @@
-#include "trajectory_generator.h"
 #include "csv_writer.h"
+#include "trajectory_generator.h"
 
 int main()
 {
-    TrajectoryGenerator generator;
+	TrajectoryGenerator generator;
 
-    auto path = generator.generateStraightLine(/*duration*/ 10.0, /*dt*/ 0.1,/*vx*/2.0,/*vy*/1.0);
+	const double duration = 10.0;
 
-    saveCSV(path, "true_line_path.csv");
+	// delta t
+	const double dt = 0.1;
 
-    return 0;
+	// linear velocity
+	const double vx = 2.0;
+	const double vy = 1.0;
+
+	auto path = generator.generateStraightLine(duration, dt, vx, vy);
+
+	saveCSV(path, "true_line_path.csv");
+
+	return 0;
 }
